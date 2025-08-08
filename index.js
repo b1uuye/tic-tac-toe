edb1311c967e968cfe6dbd4fbb39abab852e87e2
ali <alichocr2@gmail.com>
2025-08-08 11:01:33 +0100

    add player, computer and board classes

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const board = {
-    A1: '',
-    A2: '',
-    A3: '',
-    B1: '',
-    B2: '',
-    B3: '',
-    C1: '',
-    C2: '',
-    C3:  ''
-}
-
+const prompt = require("prompt-sync")({sigint:true});
 
 class Player {
     constructor(){
@@ -19,6 +8,60 @@ class Player {
 
 class HumanPlayer extends Player{
     constructor(names){
-
+        super()
+    }
+    select(options){
+        let selection = prompt("Please enter an option: ").toUpperCase()
+        let selected = false
+        while (selected === false)
+            if(options.includes(selection) === false){
+                selection = prompt("Please enter a valid option: ").toUpperCase()
+            }
+            else{
+                selected = true
+            }
+        return selection 
     }
 }
+
+class ComputerPlayer extends Player {
+    constructor() {
+        super()
+    }
+    select(options){
+    return options[Math.floor(Math.random() * options.length)]
+    }
+}
+
+class Board {
+    constructor(){
+    }
+    boardSquares = {
+    A1: '-',
+    A2: '-',
+    A3: '-',
+    B1: '-',
+    B2: '-',
+    B3: '-',
+    C1: '-',
+    C2: '-',
+    C3: '-'
+    }
+    createGame(){
+        let options = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
+        let player = new HumanPlayer()
+        let computer = new ComputerPlayer()
+        console.log(`
+        ${this.boardSquares.A1} | ${this.boardSquares.A2} | ${this.boardSquares.A3}
+        ${this.boardSquares.B1} | ${this.boardSquares.B2} | ${this.boardSquares.B3}
+        ${this.boardSquares.C1} | ${this.boardSquares.C2} | ${this.boardSquares.C3}
+        `)
+        let playerSelection = player.select(options)
+        let computerSelection = computer.select(options)
+        return
+    }
+
+}
+
+let board = new Board()
+board.createGame()
