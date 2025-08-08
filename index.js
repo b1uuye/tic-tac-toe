4849b672bdb80a636b5ed1afe750ee960df07fa4
b1uuye <onibudoi87@gmail.com>
2025-08-08 09:57:03 +0100

    Created board and player classes

---
+++ index.js
@@ -0,0 +1,24 @@
+const board = {
+    A1: '',
+    A2: '',
+    A3: '',
+    B1: '',
+    B2: '',
+    B3: '',
+    C1: '',
+    C2: '',
+    C3:  ''
+}
+
+
+class Player {
+    constructor(){
+
+    }
+}
+
+class HumanPlayer extends Player{
+    constructor(name){
+
+    }
+}
